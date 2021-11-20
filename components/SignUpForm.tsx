b7bea4ae877452d5f4c2abb08d8b5d445913be91
Ivan Esmantovich <ivanesmantovich@gmail.com>
2021-11-20 18:23:57 +0300

    Remade sign-up form with material-ui

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -1,15 +1,56 @@
 import React from 'react';
-import { Formik, Form } from 'formik';
-import TextField from './TextField';
+import { Formik, Form, Field, useField, FieldAttributes } from 'formik';
 import * as Yup from 'yup';
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
+import { FieldHookConfig } from 'formik';
+
+type MyRadioProps = { label: string } & FieldAttributes<{}>; // FieldAttributes для того чтобы передавать пропы в кастомный компонент
+
+const MyRadio: React.FC<MyRadioProps> = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <FormControlLabel {...field} control={<Radio />} label={label} /> // {...field} вставит value={field.value} onChange={field.onChange} и все остальные параметры принадлежащие field
+  );
+};
+
+const MyTextField: React.FC<FieldAttributes<{}>> = ({
+  placeholder,
+  ...props
+}) => {
+  const [field, meta] = useField<{}>(props);
+  const errorText = meta.error && meta.touched ? meta.error : '';
+
+  return (
+    <TextField
+      variant="outlined"
+      fullWidth
+      label={placeholder}
+      {...field}
+      helperText={errorText}
+      error={!!errorText}
+      type={props.type}
+    />
+  );
+};
 
 type signUpFormType = {};
 
 export const SignUpForm = ({}: signUpFormType) => {
-  const validate = Yup.object({
+  // Validation
+  const validationSchema = Yup.object({
     email: Yup.string().email('Email is invalid').required('Required'),
-    name: Yup.string()
-      .max(40, 'Must be 40 characters or less')
+    firstName: Yup.string()
+      .max(25, 'Must be 25 characters or less')
+      .required('Required'),
+    lastName: Yup.string()
+      .max(30, 'Must be 30 characters or less')
       .required('Required'),
     password: Yup.string()
       .min(6, 'Password must be at least 6 characters')
@@ -22,6 +63,8 @@ export const SignUpForm = ({}: signUpFormType) => {
       .required('Required'),
     age: Yup.number(),
   });
+  // Validation
+
   return (
     <>
       <header>
@@ -32,33 +75,94 @@ export const SignUpForm = ({}: signUpFormType) => {
         <Formik
           initialValues={{
             email: '',
-            name: '',
+            firstName: '',
+            lastName: '',
             password: '',
             gender: '',
             age: '',
           }}
-          validationSchema={validate}
-          onSubmit={(values) => {
-            console.log(values);
+          onSubmit={(data, { setSubmitting }) => {
+            // Вызывается на сабмите формы, в data содержатся поля на момент сабмита
+            setSubmitting(true);
+            // Делаю async вызов/реквест
+            // ...
+            // Делаю async вызов/реквест
+            setSubmitting(false);
+            console.log('submit:', data);
           }}
+          validationSchema={validationSchema}
         >
-          {(props) => (
-            // <div>
-            <Form onSubmit={props.handleSubmit}>
-              <TextField name="email" type="email" />
-              <TextField name="name" type="text" />
-              <TextField name="password" type="password" />
-              <TextField name="confirmPassword" type="password" />
-              <TextField name="gender" type="text" />
-              <TextField name="age" type="number" />
-              <button
-                type="submit"
-                className="font-bold uppercase bg-purple-700 hover:bg-purple-900 text-gray-50 rounded-full py-2 px-3 cursor-pointer tracking-wider"
-              >
-                Register
-              </button>
+          {(
+            { values, isSubmitting } // В values передается обьект с текущим состояним формы
+          ) => (
+            <Form>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <MyTextField placeholder="First Name" name="firstName" />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <MyTextField placeholder="Last Name" name="lastName" />
+                </Grid>
+                <Grid item xs={12}>
+                  <MyTextField placeholder="Email" name="email" />
+                </Grid>
+                <Grid item xs={12}>
+                  <MyTextField
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <MyTextField
+                    placeholder="Confirm password"
+                    name="confirmPassword"
+                    type="password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <MyTextField placeholder="Age" name="age" type="number" />
+                </Grid>
+                <Grid item xs={12}>
+                  <div>
+                    <MyRadio
+                      name="gender"
+                      value="m"
+                      type="radio"
+                      label="Male"
+                    />
+                  </div>
+                  <div>
+                    <MyRadio
+                      name="gender"
+                      value="f"
+                      type="radio"
+                      label="Female"
+                    />
+                  </div>
+                  <div>
+                    <MyRadio
+                      checked={true}
+                      name="gender"
+                      value="o"
+                      type="radio"
+                      label="Other"
+                    />
+                  </div>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    variant="contained"
+                    disableElevation
+                    size="large"
+                    type="submit"
+                  >
+                    Submit
+                  </Button>
+                </Grid>
+              </Grid>
+              <pre>{JSON.stringify(values, null, 2)}</pre>
             </Form>
-            // </div>
           )}
         </Formik>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `"Rubik", "Roboto", "Helvetica", "Arial", sans-serif`,
+  },
+  palette: {
+    primary: {
+      main: '#6D28D9'
+    }
+  }
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
