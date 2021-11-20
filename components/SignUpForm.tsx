30d0e34b2a606e35d298f87caf89cc4fcafc0240
Ivan Esmantovich <ivanesmantovich@gmail.com>
2021-11-20 08:49:33 +0300

    Added sign-up form

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import TextField from './TextField';
+import * as Yup from 'yup';
 
 type signUpFormType = {};
 
 export const SignUpForm = ({}: signUpFormType) => {
+  const validate = Yup.object({
+    email: Yup.string().email('Email is invalid').required('Required'),
+    name: Yup.string()
+      .max(40, 'Must be 40 characters or less')
+      .required('Required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 characters')
+      .required('Required'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Passwords must match')
+      .required('Required'),
+    gender: Yup.string()
+      .max(20, 'Must be 20 characters or less')
+      .required('Required'),
+    age: Yup.number(),
+  });
   return (
     <>
       <header>
@@ -20,13 +37,28 @@ export const SignUpForm = ({}: signUpFormType) => {
             gender: '',
             age: '',
           }}
+          validationSchema={validate}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
         >
-          {(formik) => (
-            <div>
-              <Form>
-                <TextField name="name" type="text" placeholder='Your Name' />
-              </Form>
-            </div>
+          {(props) => (
+            // <div>
+            <Form onSubmit={props.handleSubmit}>
+              <TextField name="email" type="email" />
+              <TextField name="name" type="text" />
+              <TextField name="password" type="password" />
+              <TextField name="confirmPassword" type="password" />
+              <TextField name="gender" type="text" />
+              <TextField name="age" type="number" />
+              <button
+                type="submit"
+                className="font-bold uppercase bg-purple-700 hover:bg-purple-900 text-gray-50 rounded-full py-2 px-3 cursor-pointer tracking-wider"
+              >
+                Register
+              </button>
+            </Form>
+            // </div>
           )}
         </Formik>
       </div>
--- components/TextField.tsx
+++ components/TextField.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import { FieldHookConfig, useField } from 'formik'
+import React from 'react';
+import { ErrorMessage, FieldHookConfig, useField } from 'formik';
 
 const TextField = (props: FieldHookConfig<string>) => {
-    const [field] = useField(props);
-    return (
-        <div>
-            <input {...field} placeholder={props.placeholder} type={props.type} />
-        </div>
-    )
-}
+  const [field, meta] = useField(props);
+  return (
+    <div className="mb-5">
+      <label
+        className={'flex flex-col font-semibold'}
+        style={{ textTransform: 'capitalize' }}
+        htmlFor={field.name}
+      >
+        {field.name}
+      </label>
+      <input
+        className={`bg-gray-100 border-b-2 outline-none mr-3 ${
+          meta.touched && meta.error ? 'border-red-700' : 'border-gray-700'
+        }`}
+        {...field}
+        type={props.type}
+      />
+      <ErrorMessage component="div" name={field.name} />
+    </div>
+  );
+};
 
-export default TextField
+export default TextField;
