7d6f6392d7dc3062f37b670da5cfca86bebb7b58
Ivan Esmantovich <ivanesmantovich@gmail.com>
2021-12-14 16:45:26 +0300

    Fixed session list (onSnapshot)

--- components/MovieSessions.tsx
+++ components/MovieSessions.tsx
@@ -18,13 +18,25 @@ const MovieSessions = ({ movieId }: MovieSessionsType) => {
 
   const q = query(sessionsRef, where('movieId', '==', movieId));
 
-  useEffect(() => {
-    const getSessions = async () => {
-      const data = await getDocs(q);
-      setMovieSessions(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
+  //   useEffect(() => {
+  //     const getSessions = async () => {
+  //       const data = await getDocs(q);
+  //       setMovieSessions(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //     };
 
-    getSessions();
+  //     getSessions();
+  //   }, []);
+
+  useEffect(() => {
+    onSnapshot(q, (querySnapshot) => {
+      const newSessions = [];
+      querySnapshot.forEach((doc) => {
+        newSessions.push({ ...doc.data(), id: doc.id });
+      });
+      console.log('Age diffs: ', newSessions);
+      setMovieSessions(newSessions);
+      console.log('movieSessions:', movieSessions);
+    });
   }, []);
 
   return (
@@ -32,7 +44,7 @@ const MovieSessions = ({ movieId }: MovieSessionsType) => {
       <div>Movie({movieId}) Sessions:</div>
       {movieSessions.map((movieSession) => {
         return (
-          <div key={movieSession.id}>
+          <div>
             <span>{movieSession.ageDiff}</span>
           </div>
         );
